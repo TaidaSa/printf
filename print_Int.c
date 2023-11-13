56068a4daac4c609be4b6fd435ca20c4892f3a1a
Abdelrahman Arafa Ibrahim <bedo161999@gmail.com>
2023-11-13 20:41:25 +0000

    printf

---
+++ print_Int.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _print_Int - print int.
+ *
+ * @args: argument.
+ *
+ * Return: count of characters.
+ */
+
+int _print_Int(va_list args)
+{
+    int dc = 1, count =0;
+    long int num = va_arg(args, int), numx;
+
+    if (num < 0)
+    {
+        count += _myPutChar('-');
+        num *= -1;
+    }
+
+    if (num < 10)
+        return(count += _myPutChar(num + '0'))
+    
+    numx = num;
+
+    while (numx > 9)
+    {
+        dc *=10;
+        numx /=10;
+    }
+
+    while (dc >= 1)
+    {
+        count += _myPutChar(((num / dc) % 10) + '0');
+        dc /= 10;
+    }
+    
+    
+    return(count);
+    
+}
