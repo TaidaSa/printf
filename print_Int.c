e97ed212c6c5622c271cde67ae266fe5c013831e
Abdelrahman Arafa Ibrahim <bedo161999@gmail.com>
2023-11-13 20:42:39 +0000

    printf

--- print_Int.c
+++ print_Int.c
@@ -10,33 +10,31 @@
 
 int _print_Int(va_list args)
 {
-    int dc = 1, count =0;
-    long int num = va_arg(args, int), numx;
+	int dc = 1, count = 0;
+	long int num = va_arg(args, int), numx;
 
-    if (num < 0)
-    {
-        count += _myPutChar('-');
-        num *= -1;
-    }
+	if (num < 0)
+	{
+		count += _myPutChar('-');
+		num *= -1;
+	}
 
-    if (num < 10)
-        return(count += _myPutChar(num + '0'))
-    
-    numx = num;
+	if (num < 10)
+		return (count += _myPutChar(num + '0'))
 
-    while (numx > 9)
-    {
-        dc *=10;
-        numx /=10;
-    }
+				   numx = num;
 
-    while (dc >= 1)
-    {
-        count += _myPutChar(((num / dc) % 10) + '0');
-        dc /= 10;
-    }
-    
-    
-    return(count);
-    
+	while (numx > 9)
+	{
+		dc *= 10;
+		numx /= 10;
+	}
+
+	while (dc >= 1)
+	{
+		count += _myPutChar(((num / dc) % 10) + '0');
+		dc /= 10;
+	}
+
+	return (count);
 }
